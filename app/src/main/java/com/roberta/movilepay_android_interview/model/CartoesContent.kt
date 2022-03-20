b6f04733f624388c917a1fa28613192dc48beef2
RobertaMaria <robertaji26@gmail.com>
2022-03-20 12:50:43 -0300

    create model

---
+++ app/src/main/java/com/roberta/movilepay_android_interview/model/CartoesContent.kt
@@ -0,0 +1,8 @@
+package com.roberta.movilepay_android_interview.model
+
+data class CartoesContent(
+     val title: String?,
+     val cardNumber: String?,
+     val button: CardsButton?,
+     val balance: CardsBalance?
+)
