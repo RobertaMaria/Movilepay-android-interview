31e245e4508b31dde39b90b3f199ba31dabb2409
RobertaMaria <robertaji26@gmail.com>
2022-03-20 20:56:17 -0300

    create extension

---
+++ app/src/main/java/com/roberta/movilepay_android_interview/extensions/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.roberta.movilepay_android_interview.extensions
+
+import android.view.View
+
+fun View.show(){
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide(){
+    this.visibility = View.GONE
+}
